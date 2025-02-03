0205e1532b5536e3d77e86424cbc098ed76cb8a7
julia8024 <julia8024@naver.com>
2025-02-04 01:48:22 +0900

    Rename: Rename Project Name

---
+++ AnyRecord/View/RecordDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordDetailView.swift
+//  AnyRecord
+//
+//  Created by 장세희 on 2/3/25.
+//
+
+import SwiftUI
+
+struct RecordDetailView: View {
+    var body: some View {
+        Text("레코드 상세뷰")
+    }
+}
+
+#Preview {
+    RecordDetailView()
+}
